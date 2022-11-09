dc711ace189b41f02bf70661d9628cb588f89a9d
MAC AIR <wonji@MACui-MacBookAir.local>
2022-11-09 15:10:37 +0900

    포트폴리오 ver.03

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(window).on("scroll",function(){
 
     if(scTop >= $(".aboutMe").offset().top){
         $(".pager").addClass("on");
-        $(".circle").addClass("on");
+        $(".circle_wrap").addClass("on");
     }
     else{
         $(".pager").removeClass("on");
@@ -47,29 +47,57 @@ $(".scBtn").on("click",function(e){
 
 });
 
-const conts = document.querySelectorAll(".cont");
-let scrollPoint = 0; //휠 내리고 올렸을 떄 이전,이후 위치값 변수
+//카운팅 될 숫자값 배열로 저장
+let countArray = [90,90,80,70,65,80,70,80];
 
-conts.forEach(function(items,index){
-    items.addEventListener("wheel",function(e){
+//배열 안에 있는 갯수만큼 반복문으로 숫자 카운팅 작업
+for(let i=0; i < countArray.length; i++){
 
+    //카운팅 기능은 함수를 만들어서 작업 (카운팅될숫자값,circle순번값)
+    autoCount(countArray[i],i);
+} 
 
+//카운팅되는 기능 함수로 정의
+function autoCount(result,index){ //매개변수 받아주기 result,index
+    //초기값 숫자
+    let start = 0;
 
-        if(e.wheelDelta > 0){
-            if(index != 0){
-                scrollPoint = e.currentTarget.previousElementSibling.offsetTop;
-            }
+    //숫자 카운팅 setInterval 구간
+    let plus = setInterval(function(){
+        start += 1; //1씩 숫자 증가
+        if(start >= result){
+            clearInterval(plus); //숫자 카운팅 중지
+            document.querySelectorAll(".circle")[index].querySelector(".count").innerHTML = result + "%";
         }
-        else if(e.wheelDelta < 0){
-            if(index != conts.length-1){
-                scrollPoint = e.currentTarget.nextElementSibling.offsetTop;
-            }
+        else{
+            document.querySelectorAll(".circle")[index].querySelector(".count").innerHTML = start + "%";
         }
+    },20);
+}
+
+// const conts = document.querySelectorAll(".cont");
+// let scrollPoint = 0; //휠 내리고 올렸을 떄 이전,이후 위치값 변수
+
+// conts.forEach(function(items,index){
+//     items.addEventListener("wheel",function(e){
+
+
+
+//         if(e.wheelDelta > 0){
+//             if(index != 0){
+//                 scrollPoint = e.currentTarget.previousElementSibling.offsetTop;
+//             }
+//         }
+//         else if(e.wheelDelta < 0){
+//             if(index != conts.length-1){
+//                 scrollPoint = e.currentTarget.nextElementSibling.offsetTop;
+//             }
+//         }
         
-        window.scrollTo({
-            top:scrollPoint,
-            behavior:"smooth"
-        });
+//         window.scrollTo({
+//             top:scrollPoint,
+//             behavior:"smooth"
+//         });
        
-    });
-});
+//     });
+// });
